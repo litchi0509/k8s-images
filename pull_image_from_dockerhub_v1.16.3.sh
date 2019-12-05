7888856cd5050ea85cbcb62de3773d0d2a0b4cd3
jackfruit <37534874+litchi0509@users.noreply.github.com>
2019-12-05 23:29:14 +0800

    Update pull_image_from_dockerhub_v1.16.3.sh

--- pull_image_from_dockerhub_v1.16.3.sh
+++ pull_image_from_dockerhub_v1.16.3.sh
@@ -4,13 +4,13 @@ gcr_name=k8s.gcr.io
 myhub_name=litchi0509
 # define images 
 images=(
- k8s.gcr.io/kube-apiserver:v1.16.3
- k8s.gcr.io/kube-controller-manager:v1.16.3
- k8s.gcr.io/kube-scheduler:v1.16.3
- k8s.gcr.io/kube-proxy:v1.16.3
- k8s.gcr.io/pause:3.1
- k8s.gcr.io/etcd:3.3.15-0
- k8s.gcr.io/coredns:1.6.2
+ kube-apiserver:v1.16.3
+ kube-controller-manager:v1.16.3
+ kube-scheduler:v1.16.3
+ kube-proxy:v1.16.3
+ pause:3.1
+ etcd:3.3.15-0
+ coredns:1.6.2
 )
 
 for image in ${images[@]}; do 
