8f36548dd6b1c6fab3e8fefe10d3b5f87fd07d07
jackfruit <37534874+litchi0509@users.noreply.github.com>
2019-12-05 23:52:06 +0800

    Create pull_image_from_dockerhub_aliyun_v1.16.3.sh

---
+++ pull_image_from_dockerhub_aliyun_v1.16.3.sh
@@ -0,0 +1,21 @@
+  
+#!/bin/bash
+
+gcr_name=k8s.gcr.io
+myhub_name=registry.cn-hangzhou.aliyuncs.com/google_containers
+# define images 
+images=(
+ kube-apiserver:v1.16.3
+ kube-controller-manager:v1.16.3
+ kube-scheduler:v1.16.3
+ kube-proxy:v1.16.3
+ pause:3.1
+ etcd:3.3.15-0
+ coredns:1.6.2
+)
+
+for image in ${images[@]}; do 
+	docker pull $myhub_name/$image
+	docker tag  $myhub_name/$image $gcr_name/$image
+	docker rmi $myhub_name/$image
+done
