ddd9399e1307f967cdb38a802d11dc3c72190c27
litchi0509 <37534874+litchi0509@users.noreply.github.com>
2020-04-24 15:08:33 +0800

    Update pull_image_from_dockerhub_aliyun_v1.16.3.sh

--- pull_image_from_dockerhub_aliyun_v1.16.3.sh
+++ pull_image_from_dockerhub_aliyun_v1.16.3.sh
@@ -4,7 +4,7 @@
 gcr_name=k8s.gcr.io
 myhub_name=registry.cn-hangzhou.aliyuncs.com/google_containers
 KUBE_DASHBOARD_VERSION=v1.10.1
-KUBE_VERSION=v1.18.1
+KUBE_VERSION=v1.18.2
 KUBE_PAUSE_VERSION=3.2
 ETCD_VERSION=3.4.3-0
 DNS_VERSION=1.6.7
