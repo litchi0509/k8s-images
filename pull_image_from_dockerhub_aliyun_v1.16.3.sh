33c9c654f9ac3898601c977effe2a5b49c47dcf8
litchi0509 <37534874+litchi0509@users.noreply.github.com>
2020-04-24 11:36:33 +0800

    Update pull_image_from_dockerhub_aliyun_v1.16.3.sh

--- pull_image_from_dockerhub_aliyun_v1.16.3.sh
+++ pull_image_from_dockerhub_aliyun_v1.16.3.sh
@@ -3,6 +3,7 @@
 
 gcr_name=k8s.gcr.io
 myhub_name=registry.cn-hangzhou.aliyuncs.com/google_containers
+KUBE_DASHBOARD_VERSION=v1.10.1
 KUBE_VERSION=v1.18.1
 KUBE_PAUSE_VERSION=3.2
 ETCD_VERSION=3.4.3-0
@@ -10,7 +11,7 @@ DNS_VERSION=1.6.7
 
 # define images 
 images=(
- kubernetes-dashboard-amd64:v1.10.1
+ kubernetes-dashboard-amd64:${KUBE_DASHBOARD_VERSION}
  kube-apiserver:${KUBE_VERSION}
  kube-controller-manager:${KUBE_VERSION}
  kube-scheduler:${KUBE_VERSION}
