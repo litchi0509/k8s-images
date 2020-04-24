f88fc5c79ea5949a03a1d1b66d0d39c0dcb8f4d5
litchi0509 <37534874+litchi0509@users.noreply.github.com>
2020-04-24 11:33:56 +0800

    Update pull_image_from_dockerhub_aliyun_v1.16.3.sh

--- pull_image_from_dockerhub_aliyun_v1.16.3.sh
+++ pull_image_from_dockerhub_aliyun_v1.16.3.sh
@@ -3,16 +3,21 @@
 
 gcr_name=k8s.gcr.io
 myhub_name=registry.cn-hangzhou.aliyuncs.com/google_containers
+KUBE_VERSION=v1.18.1
+KUBE_PAUSE_VERSION=3.2
+ETCD_VERSION=3.4.3-0
+DNS_VERSION=1.6.7
+
 # define images 
 images=(
  kubernetes-dashboard-amd64:v1.10.1
- kube-apiserver:v1.18.1
- kube-controller-manager:v1.18.1
- kube-scheduler:v1.18.1
- kube-proxy:v1.18.1
- pause:3.2
- etcd:3.4.3-0
- coredns:1.6.7
+ kube-apiserver:${KUBE_VERSION}
+ kube-controller-manager:${KUBE_VERSION}
+ kube-scheduler:${KUBE_VERSION}
+ kube-proxy:${KUBE_VERSION}
+ pause:${KUBE_PAUSE_VERSION}
+ etcd:${ETCD_VERSION}
+ coredns:${DNS_VERSION}
 )
 
 for image in ${images[@]}; do 
